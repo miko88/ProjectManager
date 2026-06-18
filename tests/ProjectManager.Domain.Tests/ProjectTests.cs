124c0e84a9ac4379738eb91c48f10c8bbceaf493
Michal Bruchanik <michal.bruchanik@gmail.com>
2026-06-18 22:50:04 +0200

    feat(domain): Project entity with invariants

---
+++ tests/ProjectManager.Domain.Tests/ProjectTests.cs
@@ -0,0 +1,48 @@
+using FluentAssertions;
+using ProjectManager.Domain;
+using Xunit;
+
+namespace ProjectManager.Domain.Tests;
+
+public class ProjectTests
+{
+    [Fact]
+    public void Create_WithValidValues_TrimsAndAssigns()
+    {
+        var p = Project.Create("prj1", "  Name  ", " ABC ", " Cust ");
+
+        p.Id.Should().Be("prj1");
+        p.Name.Should().Be("Name");
+        p.Abbreviation.Should().Be("ABC");
+        p.Customer.Should().Be("Cust");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(null)]
+    public void Create_WithBlankName_Throws(string? name)
+    {
+        var act = () => Project.Create("prj1", name!, "ABC", "Cust");
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Create_WithBlankId_Throws()
+    {
+        var act = () => Project.Create("  ", "Name", "ABC", "Cust");
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Update_ChangesMutableFields_KeepsId()
+    {
+        var p = Project.Create("prj1", "Old", "OLD", "OldCust");
+        p.Update("New", "NEW", "NewCust");
+
+        p.Id.Should().Be("prj1");
+        p.Name.Should().Be("New");
+        p.Abbreviation.Should().Be("NEW");
+        p.Customer.Should().Be("NewCust");
+    }
+}
